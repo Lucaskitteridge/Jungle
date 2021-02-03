cbe464525f2fedfb9e3d8f803ae45643a3887177
Lucaskitteridge <Lucas.benjamin.kitteridge@gmail.com>
2021-02-03 20:40:56 +0000

    dashboard admin page rendered w/ link to products

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USER'].to_s, password: ENV['ADMIN_PASSWORD'].to_s
   def show
+    @products_count = Product.count(:all)
+    @categories_count = Category.count(:all)
   end
 end
